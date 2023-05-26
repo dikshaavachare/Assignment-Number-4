3160f947385d88f0d52f972d78bef7d3da4b57da
dikshaavachare <131612696+dikshaavachare@users.noreply.github.com>
2023-05-26 20:55:34 +0530

    Add files via upload

---
+++ WITHLAMBDA.py
@@ -0,0 +1,6 @@
+add_25 = lambda x: x + 25
+
+# Test the lambda function
+number = 10
+result = add_25(number)
+print(result)
---
+++ WITHMAPS.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5, 6, 7]
+tripled_numbers = list(map(lambda x: x * 3, numbers))
+
+# Print the tripled numbers
+print("Triple of list numbers is:")
+print(tripled_numbers)
